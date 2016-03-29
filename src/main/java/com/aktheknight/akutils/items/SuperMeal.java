6a7df9cc4fa42bca9d4ed7dbd979727dcb7b7b3f
AK <AKTheKnight.minecraft@gmail.com>
2016-03-29 21:50:09 +0100

    Fixed trees with supermeal

--- src/main/java/com/aktheknight/akutils/items/SuperMeal.java
+++ src/main/java/com/aktheknight/akutils/items/SuperMeal.java
@@ -2,11 +2,10 @@ package com.aktheknight.akutils.items;
 
 import com.aktheknight.akutils.ConfigHandler;
 import com.aktheknight.akutils.util.FixedRandom;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockSapling;
-import net.minecraft.block.IGrowable;
+import net.minecraft.block.*;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
@@ -42,29 +41,64 @@ public class SuperMeal extends Item {
     public EnumActionResult onItemUse(ItemStack stack, EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
         if (!world.isRemote) {
             BlockPos growPos;
+            Block plantBlock;
             IGrowable plant;
             int radius = ConfigHandler.SuperMealRadius;
+            int numGrowable = 0;
             for (int x = -radius; x <= radius; x++) {
                 for (int z = -radius; z <= radius; z++) {
                     growPos = pos.add(x, 0, z);
-                    Block plantBlock = world.getBlockState(growPos).getBlock();
+                    plantBlock = world.getBlockState(growPos).getBlock();
+                    System.out.println(growPos + "  :  " + plantBlock.getRegistryName());
                     if (plantBlock instanceof IGrowable) {
-                        --stack.stackSize;
+                        numGrowable++;
                         plant = (IGrowable) plantBlock;
-                        if (plant instanceof BlockSapling) {
-                            plant.grow(world, this.fixedRandom, growPos, world.getBlockState(growPos));
-                            plant.grow(world, this.fixedRandom, growPos, world.getBlockState(growPos));
+                        if (plantBlock instanceof BlockSapling) {
+                            System.out.println("sapling");
+                            for(int i = 0; i < 2; i ++) {
+                                if (world.getBlockState(growPos).getBlock() == plantBlock) {
+                                    plant.grow(world, this.fixedRandom, growPos, world.getBlockState(growPos));
+                                    try {
+                                        Thread.sleep(20);
+                                    }
+                                    catch (Exception e) {
+                                        return EnumActionResult.FAIL;
+                                    }
+                                }
+                            }
+                            System.out.println("given up on tree");
+                        }
+                        if (plant.canGrow(world, growPos, world.getBlockState(growPos), false)) {
+                            plant.grow(world, new Random(123), growPos, world.getBlockState(growPos));
                         }
                         else {
-                            if (plant.canGrow(world, growPos, world.getBlockState(growPos), false)) {
-                                plant.grow(world, new Random(123), growPos, world.getBlockState(growPos));
-                            }
-                            else {
-                                plantBlock.updateTick(world, growPos, world.getBlockState(growPos), this.fixedRandom);
-                            }
+                            plantBlock.updateTick(world, growPos, world.getBlockState(growPos), this.fixedRandom);
+                        }
+                    }
+                    if(plantBlock instanceof BlockReed) {
+                        numGrowable++;
+                        System.out.println("reed");
+                        BlockReed reed = (BlockReed) plantBlock;
+                        int i;
+                        for(i = 1; world.getBlockState(growPos.up(i)).getBlock() == reed; ++i);
+                        if (i < 4) {
+                            world.setBlockState(growPos.up(i), Blocks.reeds.getDefaultState());
+                        }
+                    }
+                    if(plantBlock instanceof BlockCactus) {
+                        numGrowable++;
+                        System.out.println("cactus");
+                        BlockCactus reed = (BlockCactus) plantBlock;
+                        int i;
+                        for(i = 1; world.getBlockState(growPos.up(i)).getBlock() == reed; ++i);
+                        if (i < 4) {
+                            world.setBlockState(growPos.up(i), Blocks.cactus.getDefaultState());
                         }
                     }
                 }
+            }
+            if (numGrowable > 0) {
+                --stack.stackSize;
             }
             return EnumActionResult.SUCCESS;
         }
